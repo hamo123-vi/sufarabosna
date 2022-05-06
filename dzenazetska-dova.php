d73d2023c27da91ebf313b57dbed48d1ce397619
Amil Valjevac <amil.valjevac@stu.ibu.edu.ba>
2022-05-06 10:09:34 +0200

    Dženazetska dova - preliminarno

---
+++ dzenazetska-dova.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title> Sufara i tedžvid osnove - zabilješke </title>
+        <link rel="stylesheet" href="src/assets/style/style.css">
+    </head>
+
+    <body>
+        <?php require_once("src/templates/header.php") ?>
+        <main>
+            <div style="text-align: center">
+                <h1>DŽENAZETSKA DOVA</h1>
+                <img alt="Dženazetska dova" src="src/assets/dzenazetskadova.jpg">
+            </div>
+        </main>
+        <script type="text/javascript" src="src/assets/js/activeToggler.js"></script>
+    </body>
+</html>
